311ccd4f0008edfa048a0867f3b9a492de2582a1
shiji paul <shijipaul2@gmail.com>
2025-02-06 19:41:55 +0100

    Adding Logging in Book Service

--- src/main/java/com/example/library/service/BookService.java
+++ src/main/java/com/example/library/service/BookService.java
@@ -1,5 +1,10 @@
 package com.example.library.service;
 
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,26 +14,32 @@ import com.example.library.repository.BookRepository;
 @Service
 public class BookService {
 	
+	private static final Logger logger = LoggerFactory.getLogger(BookService.class);
+	
 	@Autowired
 	private BookRepository bookRepository;
 	
 	
 	public Book addBook(Book book) {
+		logger.info("Adding new Book : "+book.getTitle());
 		return bookRepository.save(book);
 	}
 	
 	
 	public void deleteBook(Long bookId) {
-		 bookRepository.deleteById(bookId);//do I need to check if that book exist?
+		 logger.info("Deleting  Book with Id : "+bookId);
+		 bookRepository.deleteById(bookId);
 	}
 	
 	
 	public  Book getBook(Long bookId) {
+		logger.info("Retrieving details of  Book with ID : "+bookId);
 		return bookRepository.findById(bookId).orElseThrow(()->new RuntimeException("Book not found"));
 	}
 	
 	
 	public Book updateBook(Book book , Long bookId) {
+		logger.info("Updating  Book with ID : "+bookId);
 		Book existingBook = bookRepository.findById(bookId).orElseThrow(()->new RuntimeException("Book is not found"));
 		existingBook.setAuthor(book.getAuthor());
 		existingBook.setTitle(book.getTitle());
