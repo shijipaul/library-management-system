1ebfbfb34892bb49993933c995a4ae296d1e696d
shiji paul <shijipaul2@gmail.com>
2025-05-22 11:22:09 +0200

    Implemented Asynchronous Audit Logging with @Async so that it wont affect the response time for book services.Async is thread based and is handled by Spring task executer

--- src/main/java/com/example/library/LibraryManagementSystemApplication.java
+++ src/main/java/com/example/library/LibraryManagementSystemApplication.java
@@ -2,28 +2,14 @@ package com.example.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class LibraryManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManagementSystemApplication.class, args);
 	}
-	/*
-	 * I've implemented so far:
-	 * 
-	 * ✅ Spring Boot monolith with Book, Member, and Borrow services
-	 * 
-	 * ✅ Global Exception Handling, Validation, Logging
-	 * 
-	 * ✅ Swagger/OpenAPI
-	 * 
-	 * ✅ Spring Security with JWT and DB-backed user authentication + Role entity
-	 * 
-	 * ✅ Dockerized (multi-stage build)
-	 * 
-	 * ✅ Docker Compose (Spring Boot + MySQL + Healthcheck + .env for secrets)
-	 */
-
-
+	
 }
--- src/main/java/com/example/library/service/AuditLogsService.java
+++ src/main/java/com/example/library/service/AuditLogsService.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -20,7 +21,8 @@ public class AuditLogsService {
 	public List<BookAuditLog> getBookAuditLogs(Long bookId) {
 		return bookAuditLogRepo.findByBookId(bookId);
 	}
-
+	
+	@Async
 	public void logBookAction(Long bookId, String action) {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		String username = (auth != null && auth.isAuthenticated()) ? auth.getName() : "anonymous";
